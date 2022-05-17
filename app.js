60ef648b3295dfe1ab3906ea9561c326279f7e9a
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-05-17 19:31:06 +0300

    feat: add npm remove add package.json scripts

---
+++ app.js
@@ -0,0 +1,19 @@
+// получаем модуль Express
+const express = require("express");
+// создаем приложение
+const app = express();
+let name = process.argv[2];
+let age = process.argv[3];
+ 
+console.log("name: " + name);
+console.log("age: " + age);
+ 
+// устанавливаем обработчик для маршрута "/"
+app.get("/", function(request, response){
+ 
+    response.end("Hello from Express!");
+});
+// начинаем прослушивание подключений на 3000 порту
+app.listen(3000);
+
+// http://localhost:3000/
